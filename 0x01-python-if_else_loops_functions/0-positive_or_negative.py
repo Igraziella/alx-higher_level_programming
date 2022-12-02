b3ea7987fae5c3e6425d428a156adc985f6972b7
igraziella <igraziella2@gmail.com>
2022-12-02 05:39:09 -0800

    Positive and negative ints

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print(f"{chr(i)}", end= '')
+import random
+number = random.randint(-10, 10)
+for number in range(-10, 10):
+    if number > 0:
+        print("{:d} is positive".format(number))
+    elif number == 0:
+        print("{:d} is zero".format(number))
+    elif number < 0:
+        print("{:d} is negative \n".format(number))
