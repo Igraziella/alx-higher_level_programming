65ec555dff81c7ee22178abbc8ea94d2f62f164a
igraziella <igraziella2@gmail.com>
2022-12-02 13:27:40 -0800

    combo numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num % 10 != num / 10 and num % 10 > num / 10:
-        print("{:02d}".format(num), end='')
-        if (num / 10 < 8):
-            print(", ", end='')
+    if int(num / 10) != num % 10 and int(num / 10) < num % 10:
+        print("{}{}".format(int(num / 10), num % 10), end="")
+        if (num != 89):
+            print(", ", end="")
+            print("")
