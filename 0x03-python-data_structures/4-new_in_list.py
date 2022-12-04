ecb07d29605d638508b07a5de2fab570a8b77580
igraziella <igraziella2@gmail.com>
2022-12-04 14:11:49 -0800

    python lists

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,7 @@
 '#Python function that replaces an element in a list at '
 '#a specific position without modifying the original list '
 
+
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return list
