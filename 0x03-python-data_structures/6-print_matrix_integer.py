4f8626eda9a75e139093b1085d3f80783f089819
igraziella <igraziella2@gmail.com>
2022-12-05 11:58:45 -0800

    int matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    map(function, iterables)
-    arr = [[0, 1, 4, 2, 3],
-            [4, 2, 0, 1, 5],
-            [2, 2, 1, 0, 3],
-            [3, 4, 5, 3, 0]]
-    for i in arr:
-        print('\t'.join(map(str, i)))
+    for row in matrix:
+        for column in row:
+            print("{:d}".format(column), sep='', end='')
+            if (column != row[len(row) - 1]):
+                print(' ', end='')
+            print()
