bcc1f8e147ae4bbf4254ade0fea4ec57932dc089
igraziella <igraziella2@gmail.com>
2022-12-05 07:07:55 -0800

    python lists

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,6 +4,6 @@ def delete_at(my_list=[], idx=0):
         return my_list
 
     length = len(my_list)
-    new_list = delete_at(my_list, idx)
-    del my_list[3]
+    new_list = delete_at(my_lis, idx)
+    del my_list, idx
     return new_list
