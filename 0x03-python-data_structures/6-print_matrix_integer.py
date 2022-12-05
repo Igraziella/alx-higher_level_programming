145161b8ee260d9d10359f074c4c0350d4bb1871
igraziella <igraziella2@gmail.com>
2022-12-05 10:53:28 -0800

    int matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    map(function, iterables)
+    arr = [[0, 1, 4, 2, 3],
+            [4, 2, 0, 1, 5],
+            [2, 2, 1, 0, 3],
+            [3, 4, 5, 3, 0]]
+    for i in arr:
+        print('\t'.join(map(str, i)))
