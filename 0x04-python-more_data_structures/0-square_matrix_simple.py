d22ef1dd5e47cb47f4fdc6757ff36a9ecf7e11b8
igraziella <igraziella2@gmail.com>
2022-12-07 14:53:37 -0800

    simple matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,7 @@
 def square_matrix_simple(matrix=[]):
     square_matrix = []
     for x in range(10):
-        square_matrix.append(x**2)
-        square_matrix = (map(lambda x: x**2, range(10)))
-    return new_matrix
+       new_matrix = matrix
+       square_matrix.append(x**2)
+       square_matrix = (map(lambda x: x**2, range(10)))
+       return new_matrix
