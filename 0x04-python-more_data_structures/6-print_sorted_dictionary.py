db835ebeb06461fe42e144c4bd59853e450e59da
igraziella <igraziella2@gmail.com>
2022-12-09 12:02:03 -0800

    data structures

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    a_dictionary = {"pineapple": 2, "coconut": 1, "grape": 3}
+    a_dictionary = {"pineapple", "coconut", "grape", "berry"}
     pprint(a_dictionary)
