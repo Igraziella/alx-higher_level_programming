351c31bddf50eb8c7ccaf567dd749c84a06a8bce
igraziella <igraziella2@gmail.com>
2022-12-19 19:24:09 -0800

    exception msgs

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError, ValueError:
+    except ValueError:
+        print("{} is not an integer".format(value))
         return False
