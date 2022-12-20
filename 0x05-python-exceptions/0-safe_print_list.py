2ada5e976813466e4f710c90e07f96eee2ea2889
igraziella <igraziella2@gmail.com>
2022-12-19 20:20:08 -0800

    handling error msgs

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     for element in my_list:
         if counter < x:
             try:
-                print(f"{element}", end=''
+                print(f"{element}", end='')
                 counter += 1
             except IndexError:
                 break
