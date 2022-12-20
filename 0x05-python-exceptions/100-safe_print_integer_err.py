95fb5f895ead7501cd163383dff37f85d633b0a8
igraziella <igraziella2@gmail.com>
2022-12-20 13:02:36 -0800

    handling errors

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError) as errorr:
+    except (TypeError, ValueError) as error:
         print(f"Exception: {error}", file=sys.stderr)
         return False
