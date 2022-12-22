a334ad42ec6b9c91112b79a8e7829aca9805dadd
igraziella <igraziella2@gmail.com>
2022-12-21 23:41:22 -0800

    Task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class Square with size"""
+
+
+class Square:
+    """Class to create a square with its size"""
+
+    def __init__(self, size):
+        """Creation of the square"""
+        self.size = square_size
