1a261dbe9d0a919a6657e9f55513229d6fefd3a9
igraziella <igraziella2@gmail.com>
2022-12-21 23:44:11 -0800

    Task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,6 @@
 class Square:
     """Class to create a square with its size"""
 
-    def __init__(self, size):
+    def __init__(self, square_size):
         """Creation of the square"""
         self.size = square_size
