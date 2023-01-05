d4935212f38151a9bfe4dce4612b72f7d1390b96
igraziella <igraziella2@gmail.com>
2023-01-04 22:09:04 -0800

    rectangle 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Defines a class - Rectangle """
+
+
+def __init__(self, width=0, height=0):
+    """ Instantiation of a new rectangle """
+
+    @property
+    def width(self):
+        """ Gets the width of the rectangle. """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError(width must be an integer)
+        if value < 0:
+            raise ValueError(width must be >= 0)
+        return self.__width = value
+
+    @property
+    def height(self):
+        """ Gets the height of the rectangle """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError(height must be an integer)
+        if value < 0:
+            raise ValueError(height must be >= 0)
+        return self.__height = value
