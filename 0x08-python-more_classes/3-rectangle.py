e2a9d80b4835b02342011557115156ef8054ffb9
igraziella <igraziella2@gmail.com>
2023-01-08 07:57:52 -0800

    string repr

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -19,9 +19,9 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(width) != int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -30,9 +30,9 @@ class Rectangle:
         return self.height
 
     def height(self, value):
-        if not isinstance(value, int):
+        if type(height) != int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
