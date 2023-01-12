29d4a0e426537177cd23a8debbcb36cf7387f38d
igraziella <igraziella2@gmail.com>
2023-01-12 09:15:14 -0800

    numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-    if (num != 99):
-        print("{}{}, ".format(int(num / 10), num % 10), end="")
-    else:
-        print("{}{}".format(int(num / 10), num % 10))
+for i in range(0, 99):
+    print("{:02d}, ".format(i), end='')
+print("{}".format(99))
