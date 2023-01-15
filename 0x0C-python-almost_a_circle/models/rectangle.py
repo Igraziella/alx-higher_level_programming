7f27564bbd35a117da057b29a53dd513318c08fe
igraziella <igraziella2@gmail.com>
2023-01-15 01:43:24 -0800

    rectangle module

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,4 +79,79 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
+    def area(self):
+        """  returns the area value of the Rectangle """
+        return self.__width * self.__height
 
+    def display(self):
+        """ Prints the Rectangle using the # character """
+        if self.__width == 0 or self.__height == 0:
+            print ("")
+            return
+
+        [print("") for y in range(self.__y)]
+        for h in range(self.__height):
+            [print(" ", end="") for x in range(self.__x)]
+            [print("#", end="") for w in range(self.__width)]
+            print("")
+
+    def update(self, *args, **kwargs):
+        """ Update the Rectangle.
+
+        Args:
+            *args (ints): New attribute values.
+            1st argument represents id attribute
+            2nd argument represents width attribute
+            3rd argument represents height attribute
+            4th argument represents x attribute
+            5th argument represents y attribute        
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                    a += 1
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                    elif k == "width":
+                        self.width = v
+                    elif k == "height":
+                        self.height = v
+                    elif k == "x":
+                        self.x = v
+                    elif k == "y":
+                        self.y = v
+
+    def to dictionary(self):
+        """ returns the dictionary representation of the Rectangle """
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """ retuns the print() and string() representation of the Rectangle """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                        self.x, self.y,
+                                                        self.width, self.height)
