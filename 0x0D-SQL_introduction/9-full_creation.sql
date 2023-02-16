65c35a8c4a25b11a9d9423e63c1940790c71aaaf
igraziella <igraziella2@gmail.com>
2023-02-16 06:59:18 -0800

    modifying database

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Create a table called second_table in the hbtn_0c_0 database, add mutliple rows.
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
 INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
