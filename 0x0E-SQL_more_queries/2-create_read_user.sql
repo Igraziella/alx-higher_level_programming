0e75d3f8ece67f64cf667eac53831bd36d67b536
igraziella <igraziella2@gmail.com>
2023-02-17 23:39:24 -0800

    select privilege added

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Creates the database hbtn_0d_2 and the user user_0d_2
 -- The user should have SELECT privilege
 CREATE Database IF NOT EXISTS hbtn_0d_02;
-CREATE user IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+CREATE user IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
