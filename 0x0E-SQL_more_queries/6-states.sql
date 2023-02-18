69132a1c24df8646008a1662a00d8c3c7eeb8b39
igraziella <igraziella2@gmail.com>
2023-02-18 00:00:14 -0800

    modifying database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- Creates the database hbtn_0d_usa and the table states
+-- states id must be unique, auto generated, primary key and not null
 CREATE Database IF NOT EXISTS hbtn_0d_usa;
 CREATE Table IF NOT EXISTS hbtn_0d_usa.states (
-	id INT NOT NULL AUTO_GENERATE,
+	id INT UNIQUE NOT NULL AUTO_GENERATE,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY(id));
