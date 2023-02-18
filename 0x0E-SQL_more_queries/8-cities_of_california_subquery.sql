8c2b7424962bafd24bf44c90e8641a9f0b01ae2c
igraziella <igraziella2@gmail.com>
2023-02-18 01:05:34 -0800

    modifying database

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- Lists all cities of California found in hbtn_0d_usa database
 -- results are sorted in ascending order by cities id
 SELECT id, name FROM cities
-WHERE state_id = (
+WHERE state_id IN (
 	SELECT id FROM states
 	WHERE name = 'Carlifornia')
 ORDER BY id ASC;
