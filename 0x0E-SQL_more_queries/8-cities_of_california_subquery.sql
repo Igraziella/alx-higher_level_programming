6ebd7cbcbdd00ce41ede07ad3e777daa90ad454f
igraziella <igraziella2@gmail.com>
2023-02-18 00:25:32 -0800

    modifying database

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Lists all cities of California found in hbtn_0d_usa database
-SELECT cities, name FROM hbtn_0d_usa
+SELECT cities FROM hbtn_0d_usa.states
 WHERE name = Carlifornia
 ORDER BY cities.id  ASC;
