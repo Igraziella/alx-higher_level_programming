973eaee3b9cc204162553dd6d419fa71df847316
igraziella <igraziella2@gmail.com>
2023-02-18 01:35:48 -0800

    modifying tables

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Lists all cities contained in the hbtn_0d_usa database
 -- list is sorted in order of ascending cities.id
-SELECT c.id, c.name. s.name
+SELECT c.id, c.name, s.name
 FROM cities AS c
 INNER JOIN states AS s
 ON c.state_id = s.id
