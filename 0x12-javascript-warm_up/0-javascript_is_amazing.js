bd93b563c0719438c3e35cd01f408b590d67590d
igraziella <igraziella2@gmail.com>
2023-03-13 15:31:41 -0700

    Javascript

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// A script that prints a given sentence
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// A script thatn prints 3 lines
+
+const multiLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+console.log(multiLanguages);
