10cb162821a7b0f724563191ffcbffe739fe45d9
igraziella <igraziella2@gmail.com>
2023-03-15 13:46:03 -0700

    completed tasks

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Prints two arguments passed to it
+// using the format "is".
+
+console.log(process.argv[2] + ' is ' + process.argv[3]);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Print 3 lines using an array of string and a loop
+
+for (const line of ['C is fun', 'Python is cool', 'JacaScript is amazing']) {
+  console.log(line);
+}
