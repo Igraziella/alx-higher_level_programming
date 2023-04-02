93a830c657d04ca4f2be5fea07b4862898ebf147
igraziella <igraziella2@gmail.com>
2023-04-02 02:36:40 -0700

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     email = sys.argv[2]
 
     value = {'email'}
-    request = requests.get(url, email)
+    response = requests.post(url, value)
     html = response.read()
