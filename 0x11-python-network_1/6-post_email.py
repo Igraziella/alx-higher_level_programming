6a5a5866e17870f6a5f9f4c4f9e2319729918930
igraziella <igraziella2@gmail.com>
2023-04-02 02:28:43 -0700

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" A script that:
+- Takes in a URL and an email address
+- Sends a POST request to the passed URL with the email as a parameter
+- Displays the body of the response.
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    value = {'email'}
+    request = requests.get(url, email)
+    html = response.read()
