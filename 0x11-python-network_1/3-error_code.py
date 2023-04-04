2ecdbdf7dbce70d6e973fb514dae6ca9e8e41607
igraziella <igraziella2@gmail.com>
2023-04-04 12:31:14 -0700

    completed task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" A script that:
-- takes in a URL,
-- sends a request to the URL
+""" A script thattakes in a URL,
+- sends a request to the URL, and
 - displays the body of the response (decoded in utf-8).
 """
+import sys
+import urllib
 
 
 if __name__ == "__main__":
-    import sys
-    from urllib import request, error
 
     try:
         with request.urlopen(sys.argv[1]) as response:
