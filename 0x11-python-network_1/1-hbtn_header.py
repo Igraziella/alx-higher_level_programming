00a1ef04778bd8f4cbeb00cb523726bf82166687
igraziella <igraziella2@gmail.com>
2023-04-04 14:10:51 -0700

    completed task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,6 +11,5 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(url) as response:
+        print((response.headers).get("X-Request-Id"))
