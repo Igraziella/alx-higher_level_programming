982b4475833d3dee66a37f0732b98f74d05d74bf
igraziella <igraziella2@gmail.com>
2023-04-04 12:22:20 -0700

    completed task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
+""" A script that takes in a URL,
 - sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header of the response.
 """
 import sys
 import urllib.request
 
+
 if __name__ == "__main__":
+
     url = sys.argv[1]
 
     request = urllib.request.Request(url)
