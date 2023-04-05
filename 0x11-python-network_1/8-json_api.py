e7c248a81a45954008065a4142498aa36f1d55f8
igraziella <igraziella2@gmail.com>
2023-04-05 00:11:58 -0700

    completed task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Takes in a letter,
+- Sends a POST request to http://0.0.0.0:5000/search_user
+- with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    letter = sys.argv[1] if len(sys.argv) == 1: else letter = ""
+
+    url = "http://0.0.0.0:5000/search_user"
+    value = {"q": letter}
+
+    r = request.post(url, data=value)
+
+    try:
+        response = r.json()
+        if response = {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
