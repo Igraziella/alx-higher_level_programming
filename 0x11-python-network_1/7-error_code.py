6f17b8fda6e5b9f45742ae1b7a1f14171659ee48
igraziella <igraziella2@gmail.com>
2023-04-05 03:22:00 -0700

    completed task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,11 +10,10 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    try:
-        response = requests.get(url)
-        if response.status_code >= 400:
-            print("Error code:{}".format(response.status_code))
-        else:
-            print(response.status_code)
-    except Exception as ex:
-        print(f"Unable to connect to server, with error :{ex}")
+#    print(url)
+    
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code:{}".format(response.status_code))
+    else:
+        print(response.text)
