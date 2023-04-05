9efde82dc39d7ba6e28bb810eac11fc147afc17c
igraziella <igraziella2@gmail.com>
2023-04-05 03:29:47 -0700

    completed task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,10 +9,9 @@ import requests
 
 
 if __name__ == "__main__":
-
     url = sys.argv[1]
     print(url)
-    
+
     response = requests.get(url)
     if response.status_code >= 400:
         print("Error code:{}".format(response.status_code))
