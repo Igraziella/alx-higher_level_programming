339f7613ed77116829b6767a10003ac7bcc5e310
igraziella <igraziella2@gmail.com>
2023-04-25 08:31:04 -0700

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// A script that writes a string to a file.
+
+const fs = require('fs');
+const content = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(content, string, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
